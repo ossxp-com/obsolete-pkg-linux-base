d641df8a355cd4e68eb09dc3ccaa64def4b5957d
Jiang Xin <worldhello.net AT gmail DOT com>
2007-04-11 01:39:57 +0000

    run config in postinst

--- ossxp-linux/debian/base.sh
+++ ossxp-linux/debian/base.sh
@@ -28,149 +28,161 @@ function set_config()
     fi
 }
 
-# tasksel standard package
-#------------------------------------------------------------
-# aptitude search ~pstandard
-echo -e "[1minstall ~pstandard :[0m"
-aptitude install -y ~pstandard || echo -e "[1m[44minstall ~pstandard failed! [0m"
-# aptitude search ~prequired
-echo -e "[1minstall ~prequired :[0m"
-aptitude install -y ~prequired || echo -e "[1m[44minstall ~prequired failed! [0m"
-# aptitude search ~pimportant:
-echo -e "[1minstall ~pimportant :[0m"
-aptitude install -y ~pimportant || echo -e "[1m[44minstall ~pimportant failed! [0m"
+function do_install()
+{
+    # tasksel standard package
+    #------------------------------------------------------------
+    # aptitude search ~pstandard
+    echo -e "[1minstall ~pstandard :[0m"
+    aptitude install -y ~pstandard || echo -e "[1m[44minstall ~pstandard failed! [0m"
+    # aptitude search ~prequired
+    echo -e "[1minstall ~prequired :[0m"
+    aptitude install -y ~prequired || echo -e "[1m[44minstall ~prequired failed! [0m"
+    # aptitude search ~pimportant:
+    echo -e "[1minstall ~pimportant :[0m"
+    aptitude install -y ~pimportant || echo -e "[1m[44minstall ~pimportant failed! [0m"
 
-# install packages
-#------------------------------------------------------------
-# My already installed by tasksel standard package:
-apt-get install --force-yes -y less locales wget || true
+    # install packages
+    #------------------------------------------------------------
+    # My already installed by tasksel standard package:
+    apt-get install --force-yes -y less locales wget ssh sudo || true
+    
+    for pkg in \
+        acl bsdutils bzip2 \
+        flexbackup \
+        ntpdate \
+        openssl p7zip-full pciutils psmisc rdiff-backup \
+        screen shellutils ssh star sudo sysutils \
+        unison vim \
+    ; do
+        echo -e "[1minstall $pkg :[0m"
+        apt-get install --force-yes -y $pkg || echo -e "[1m[44minstall $pkg failed! [0m"
+    done
+    }
 
-for pkg in \
-    acl bsdutils bzip2 \
-    flexbackup \
-    ntpdate \
-    openssl p7zip-full pciutils psmisc rdiff-backup \
-    screen shellutils ssh star sudo sysutils \
-    unison vim \
-; do
-    echo -e "[1minstall $pkg :[0m"
-    apt-get install --force-yes -y $pkg || echo -e "[1m[44minstall $pkg failed! [0m"
-done
 
-#------------------------------------------------------------
-# set command line style to vi (/etc/inputrc, /etc/bash.bashrc)
-CONFFILE=/etc/inputrc
-if [ -f ${CONFFILE} ]; then
-    if ! grep -iq "OSSXP.COM settings:" ${CONFFILE}; then
-        echo -e "[1msetting ${CONFFILE}...	done[0m"
-        cat >> ${CONFFILE} << EOF
+function do_config()
+{
+    #------------------------------------------------------------
+    # set command line style to vi (/etc/inputrc, /etc/bash.bashrc)
+    CONFFILE=/etc/inputrc
+    if [ -f ${CONFFILE} ]; then
+        if ! grep -iq "OSSXP.COM settings:" ${CONFFILE}; then
+            echo -e "[1msetting ${CONFFILE}...	done[0m"
+            cat >> ${CONFFILE} << EOF
 
 ### OSSXP.COM settings:
 set bell-style visible
 set completion-ignore-case on
 set editing-mode vi
 EOF
-    fi
-else
-    # set -o vi
-    CONFFILE=/etc/bash.bashrc
-    if [ ! -f ${CONFFILE} ]; then
-        CONFFILE=/etc/profile
-    fi
-    if [ -f ${CONFFILE} ]; then
-        if ! grep -q "^[[:space:]]*set -o vi" ${CONFFILE}; then
-            echo -e "[1msetting ${CONFFILE}...	done[0m"
-            cat >> ${CONFFILE} << EOF
+        fi
+    else
+        echo -e "[1mWarning: /etc/inputrc not exist, use /etc/profile instead![0m"
+        # set -o vi
+        CONFFILE=/etc/bash.bashrc
+        if [ ! -f ${CONFFILE} ]; then
+            CONFFILE=/etc/profile
+        fi
+        if [ -f ${CONFFILE} ]; then
+            if ! grep -q "^[[:space:]]*set -o vi" ${CONFFILE}; then
+                echo -e "[1msetting ${CONFFILE}...	done[0m"
+                cat >> ${CONFFILE} << EOF
 
 set -o vi
 EOF
+            fi
         fi
     fi
-fi
-
-
-if [ -f /etc/bash_completion ]; then
-    CONFFILE=/etc/bash.bashrc
-    if [ ! -f ${CONFFILE} ]; then
+    
+    
+    if [ -f /etc/bash_completion ]; then
+        CONFFILE=/etc/bash.bashrc
+        if [ ! -f ${CONFFILE} ]; then
         CONFFILE=/etc/profile
-    fi
-    if [ -f ${CONFFILE} ]; then
-        if ! grep -q "^[[:space:]]*[^#].*bash_completion" ${CONFFILE}; then
-            echo -e "[1menable bash_completion...	done[0m"
-            cat >> ${CONFFILE} << EOF
+        fi
+        if [ -f ${CONFFILE} ]; then
+            if ! grep -q "^[[:space:]]*[^#].*bash_completion" ${CONFFILE}; then
+                echo -e "[1menable bash_completion...	done[0m"
+                cat >> ${CONFFILE} << EOF
 
 # enable bash completion in interactive shells
 if [ -f /etc/bash_completion ]; then
     . /etc/bash_completion
 fi
 EOF
+            fi
+        else
+            echo -e "[1mWarning: ${CONFFILE} does not exist![0m"
         fi
     fi
-fi
 
-#------------------------------------------------------------
-# sudoers
-CONFFILE=/etc/sudoers
-if [ -f ${CONFFILE} ]; then
-    if ! id -g wheel > /dev/null 2>&1 ; then
-        addgroup --system wheel
-    fi
-    if ! grep -q "^%wheel" ${CONFFILE}; then
-        echo -e "[1msetting ${CONFFILE}...	done[0m"
-        cat >> ${CONFFILE} << EOF
+    #------------------------------------------------------------
+    # sudoers
+    CONFFILE=/etc/sudoers
+    if [ -f ${CONFFILE} ]; then
+        if ! id -g wheel > /dev/null 2>&1 ; then
+            addgroup --system wheel
+        fi
+        if ! grep -q "^%wheel" ${CONFFILE}; then
+            echo -e "[1msetting ${CONFFILE}...	done[0m"
+            cat >> ${CONFFILE} << EOF
 
 #User_Alias      FULLTIMERS = admin1,admin2
 #FULLTIMERS      ALL = NOPASSWD: ALL
 
 %wheel          ALL = (ALL) ALL
 EOF
+        fi
+    else
+        echo -e "[1mWarning: ${CONFFILE} does not exist![0m"
     fi
-fi
 
-
-#------------------------------------------------------------
-# sshd_config
-CONFFILE=/etc/ssh/sshd_config
-if [ -f ${CONFFILE} ]; then
-    RESTARTSSH="no"
-    if ! awk -F: '{print $1;}' /etc/group | grep -q wheel; then
-        /usr/sbin/addgroup --system wheel
-    fi
-    if ! grep -q "^[[:space:]]*[#]\?[[:space:]]*AllowGroups" ${CONFFILE}; then
-        echo -e "[1msetting ${CONFFILE}...	done[0m"
-        cat >> ${CONFFILE} << EOF
+    #------------------------------------------------------------
+    # sshd_config
+    CONFFILE=/etc/ssh/sshd_config
+    if [ -f ${CONFFILE} ]; then
+        RESTARTSSH="no"
+        if ! awk -F: '{print $1;}' /etc/group | grep -q wheel; then
+            /usr/sbin/addgroup --system wheel
+        fi
+        if ! grep -q "^[[:space:]]*[#]\?[[:space:]]*AllowGroups" ${CONFFILE}; then
+            echo -e "[1msetting ${CONFFILE}...	done[0m"
+            cat >> ${CONFFILE} << EOF
 
 # Only allow login if users belong to these groups:
 #AllowGroups wheel
 EOF
 
-        echo -ne "[1m[44m"
-        echo -e "[/etc/ssh/sshd_config]: uncomment 'AllowGroups wheel' to secure your ssh."
-        echo -ne "[0m"
-    fi
-
-    set_config Protocol 2 && RESTARTSSH="yes"
-    set_config PermitRootLogin no && RESTARTSSH="yes"
-    set_config PermitEmptyPasswords no && RESTARTSSH="yes"
-    set_config UsePrivilegeSeparation yes && RESTARTSSH="yes"
-
-    # restart ssh daemon
-    if [ "$RESTARTSSH" = "yes" ]; then
-        if [ -x /etc/init.d/ssh ]; then
-            /etc/init.d/ssh restart
-        else
-            invoke-rc.d ssh restart 
+            echo -ne "[1m[44m"
+            echo -e "[/etc/ssh/sshd_config]: uncomment 'AllowGroups wheel' to secure your ssh."
+            echo -ne "[0m"
         fi
+    
+        set_config Protocol 2 && RESTARTSSH="yes"
+        set_config PermitRootLogin no && RESTARTSSH="yes"
+        set_config PermitEmptyPasswords no && RESTARTSSH="yes"
+        set_config UsePrivilegeSeparation yes && RESTARTSSH="yes"
+    
+        # restart ssh daemon
+        if [ "$RESTARTSSH" = "yes" ]; then
+            if [ -x /etc/init.d/ssh ]; then
+                /etc/init.d/ssh restart
+            else
+                invoke-rc.d ssh restart 
+            fi
+        fi
+    else
+        echo -e "[1mWarning: ${CONFFILE} does not exist![0m"
     fi
-fi
 
-#------------------------------------------------------------
-# screenrc
-CONFFILE=/etc/screenrc
-if [ -f ${CONFFILE} ]; then
-    if ! grep -q "^caption always" ${CONFFILE}; then
-        echo -e "[1msetting ${CONFFILE}...	done[0m"
-        cat >> ${CONFFILE} << EOF
+    #------------------------------------------------------------
+    # screenrc
+    CONFFILE=/etc/screenrc
+    if [ -f ${CONFFILE} ]; then
+        if ! grep -q "^caption always" ${CONFFILE}; then
+            echo -e "[1msetting ${CONFFILE}...	done[0m"
+            cat >> ${CONFFILE} << EOF
 
 #defscrollback 3000
 #vbell on
@@ -179,49 +191,53 @@ if [ -f ${CONFFILE} ]; then
 # Set the caption on the bottom line
 caption always "%{= kw}%-Lw%{= BW}%n %t%{-}%+w %-= @%H - %Y/%m/%d, %C"
 EOF
+        fi
+    else
+        echo -e "[1mWarning: ${CONFFILE} does not exist![0m"
     fi
-fi
 
-#------------------------------------------------------------
-# locales
-CONFFILE=/etc/locale.gen
-if [ -f ${CONFFILE} ]; then
-    GENLOCALE=no
-    i="en_US.UTF-8 UTF-8"
-    grep -q "${i}" ${CONFFILE} || { echo "${i}" >> ${CONFFILE} ;  GENLOCALE=yes; }
-    i="en_US ISO-8859-1"
-    grep -q "${i}" ${CONFFILE} || { echo "${i}" >> ${CONFFILE} ;  GENLOCALE=yes; }
-    i="zh_CN.UTF-8 UTF-8"
-    grep -q "${i}" ${CONFFILE} || { echo "${i}" >> ${CONFFILE} ;  GENLOCALE=yes; }
-    i="zh_CN.GBK GBK"
-    grep -q "${i}" ${CONFFILE} || { echo "${i}" >> ${CONFFILE} ;  GENLOCALE=yes; }
-    i="zh_CN GB2312"
-    grep -q "${i}" ${CONFFILE} || { echo "${i}" >> ${CONFFILE} ;  GENLOCALE=yes; }
-    i="zh_TW.UTF-8 UTF-8"
-    grep -q "${i}" ${CONFFILE} || { echo "${i}" >> ${CONFFILE} ;  GENLOCALE=yes; }
-    i="zh_TW BIG5"
-    grep -q "${i}" ${CONFFILE} || { echo "${i}" >> ${CONFFILE} ;  GENLOCALE=yes; }
-    if [ "${GENLOCALE}" = "yes" ]; then
-        echo -e "[1msetting ${CONFFILE}...	done[0m"
-        /usr/sbin/locale-gen
-        /usr/sbin/update-locale --no-checks "LANG=zh_CN.UTF-8"
+    #------------------------------------------------------------
+    # locales
+    CONFFILE=/etc/locale.gen
+    if [ -f ${CONFFILE} ]; then
+        GENLOCALE=no
+        i="en_US.UTF-8 UTF-8"
+        grep -q "${i}" ${CONFFILE} || { echo "${i}" >> ${CONFFILE} ;  GENLOCALE=yes; }
+        i="en_US ISO-8859-1"
+        grep -q "${i}" ${CONFFILE} || { echo "${i}" >> ${CONFFILE} ;  GENLOCALE=yes; }
+        i="zh_CN.UTF-8 UTF-8"
+        grep -q "${i}" ${CONFFILE} || { echo "${i}" >> ${CONFFILE} ;  GENLOCALE=yes; }
+        i="zh_CN.GBK GBK"
+        grep -q "${i}" ${CONFFILE} || { echo "${i}" >> ${CONFFILE} ;  GENLOCALE=yes; }
+        i="zh_CN GB2312"
+        grep -q "${i}" ${CONFFILE} || { echo "${i}" >> ${CONFFILE} ;  GENLOCALE=yes; }
+        i="zh_TW.UTF-8 UTF-8"
+        grep -q "${i}" ${CONFFILE} || { echo "${i}" >> ${CONFFILE} ;  GENLOCALE=yes; }
+        i="zh_TW BIG5"
+        grep -q "${i}" ${CONFFILE} || { echo "${i}" >> ${CONFFILE} ;  GENLOCALE=yes; }
+        if [ "${GENLOCALE}" = "yes" ]; then
+            echo -e "[1msetting ${CONFFILE}...	done[0m"
+            /usr/sbin/locale-gen
+            /usr/sbin/update-locale --no-checks "LANG=zh_CN.UTF-8"
+        fi
+    else
+        echo -e "[1mWarning: ${CONFFILE} does not exist![0m"
     fi
-fi
-
-#------------------------------------------------------------
-# time settings
-CONFFILE=/etc/default/rcS
-if [ -f ${CONFFILE} ]; then
-    if grep -iq "^[[:space:]]*utc[[:space:]]*=[[:space:]]*yes" ${CONFFILE}; then
-        echo -ne "[1m[44m"
-        echo -e "Warning: [/etc/default/rcS]: UTC=yes! \nbe sure its your really wants."
-        echo -ne "[0m"
+    
+    #------------------------------------------------------------
+    # time settings
+    CONFFILE=/etc/default/rcS
+    if [ -f ${CONFFILE} ]; then
+        if grep -iq "^[[:space:]]*utc[[:space:]]*=[[:space:]]*yes" ${CONFFILE}; then
+            echo -ne "[1m[44m"
+            echo -e "Warning: [/etc/default/rcS]: UTC=yes! \nbe sure its your really wants."
+            echo -ne "[0m"
+        fi
     fi
-fi
-
-#------------------------------------------------------------
-echo -ne "[1m[44m"
-cat << EOF
+    
+    #------------------------------------------------------------
+    echo -ne "[1m[44m"
+    cat << EOF
 
 ==================================================
 * OSSXP said: something you can do with you system:
@@ -238,7 +254,35 @@ cat << EOF
         broadcast x.x.x.255
 
 EOF
-echo -ne "[0m"
+    echo -ne "[0m"
 
-[ "$0" != "${0%.sh}" ] && mv -f $0 ${0%.sh}.done
+}
+
+
+#------------------------------------------------------------
+# MAIN FUNCTION HERE
+if [ $# -eq 0 ]; then
+    do_install
+    do_config
+    [ "$0" != "${0%.sh}" ] && mv -f $0 ${0%.sh}.done
+    exit 0
+fi
+
+while [ $# -gt 0 ]; do
+    case $1 in
+    install)
+        shift
+        do_install
+    ;;
+    config)
+        shift
+        do_config
+    ;;
+    *)
+        shift
+        echo "Wrong params: $1"
+    ;;
+    esac
+done
+
 #exit 0
--- ossxp-linux/debian/misc.sh
+++ ossxp-linux/debian/misc.sh
@@ -11,37 +11,72 @@ if [ -x /opt/ossxp/install/base.sh ]; then
     sh /opt/ossxp/install/base.sh
 fi
 
-# install packages
-#------------------------------------------------------------
-# My already installed by tasksel standard package:
-apt-get install --force-yes -y nfs-common portmap || true
+function do_install()
+{
+    # install packages
+    #------------------------------------------------------------
+    # My already installed by tasksel standard package:
+    apt-get install --force-yes -y nfs-common portmap || true
 
-for pkg in \
-    apt-show-versions ascii auto-apt curl \
-    eject ethtool fping \
-    gpm indent lynx mc nmap pdumpfs \
-    smbfs sshfs tcpdump xprobe zhcon \
-; do
-    echo -e "[1minstall $pkg :[0m"
-    apt-get install --force-yes -y $pkg || echo -e "[1m[44minstall $pkg failed! [0m"
-done
+    for pkg in \
+        apt-show-versions ascii auto-apt curl \
+        eject ethtool fping \
+        gpm indent lynx mc nmap pdumpfs \
+        smbfs sshfs tcpdump xprobe zhcon \
+    ; do
+        echo -e "[1minstall $pkg :[0m"
+        apt-get install --force-yes -y $pkg || echo -e "[1m[44minstall $pkg failed! [0m"
+    done
+}
 
-#------------------------------------------------------------
-# sshfs(fuse-utils) needs this special file
-[ -c /dev/fuse ] || mknod -m 666 /dev/fuse c 10 229
-
-#------------------------------------------------------------
-# .indent.pro
-CONFFILE=/etc/skel/.indent.pro
-if [ ! -f ${CONFFILE} ]; then
-    mkdir -p /etc/skel
-    cat >> ${CONFFILE} << EOF
+function do_config()
+{
+    #------------------------------------------------------------
+    # sshfs(fuse-utils) needs this special file
+    [ -c /dev/fuse ] || mknod -m 666 /dev/fuse c 10 229
+    
+    #------------------------------------------------------------
+    # .indent.pro
+    CONFFILE=/etc/skel/.indent.pro
+    if [ ! -f ${CONFFILE} ]; then
+        mkdir -p /etc/skel
+        cat >> ${CONFFILE} << EOF
 -bad -bap -bbb -bbo -nbc -bl -bli0 -bls -c33 -cd33 -ncdb -ncdw -nce
 -cli0 -cp33 -cs -d0 -nbfda -di2 -nfc1 -nfca -hnl -ip5 -l75 -lp -pcs -nprs
 -psl -saf -sai -saw -nsc -nsob -nss -i4 -ts4 -ut
 EOF
-fi
+    fi
+
+    if [ ! -f /root/.indent.pro ]; then
+        ln -s ${CONFFILE} /root/.indent.pro
+    fi
+}
+
 
 #------------------------------------------------------------
-[ "$0" != "${0%.sh}" ] && mv -f $0 ${0%.sh}.done
-exit 0
+# MAIN FUNCTION HERE
+if [ $# -eq 0 ]; then
+    do_install
+    do_config
+    [ "$0" != "${0%.sh}" ] && mv -f $0 ${0%.sh}.done
+    exit 0
+fi
+
+while [ $# -gt 0 ]; do
+    case $1 in
+    install)
+        shift
+        do_install
+    ;;
+    config)
+        shift
+        do_config
+    ;;
+    *)
+        shift
+        echo "Wrong params: $1"
+    ;;
+    esac
+done
+
+#exit 0
