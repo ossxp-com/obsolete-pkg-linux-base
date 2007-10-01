a66d28cb218650788d00739a60055c31300e61d4
Jiang Xin <worldhello.net AT gmail DOT com>
2007-10-01 16:34:14 +0000

    add verbose/quiet options

--- ossxp-linux/debian/base/bin/vncport.py
+++ ossxp-linux/debian/base/bin/vncport.py
@@ -8,7 +8,9 @@ Usage: vncport.py [options]
 Options:
     -h|--help
             Print this message and exit.
-    -d|--display <Num>
+    -q|--quiet
+            Quiet mode.
+    -c|--check <Num>
             Check whether display #Num is availble.
     -a|--available
             Check next available display number.
@@ -22,23 +24,23 @@ def CheckDisplayNumber(n):
     n = int(n)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     if s.connect_ex(('localhost', 6000+n)) == 0:
-        print "Warning: display #%d is taken because of port %d is used." % (n,6000+n)
+        print_err ( "Warning: display #%d is taken because of port %d is used." % (n,6000+n) )
         s.close()
 	return 0
 
     if s.connect_ex(('localhost', 5900+n)) == 0:
-        print "Warning: display #%d is taken because of port %d is used." % (n,5900+n)
+        print_err ( "Warning: display #%d is taken because of port %d is used." % (n,5900+n) )
         s.close()
 	return 0
 
     s.close()
 
     if os.access("/tmp/.X%d-lock" % n, os.F_OK) :
-        print "Warning: display #%d is taken because of /tmp/.X%d-lock" % (n,n)
+        print_err ( "Warning: display #%d is taken because of /tmp/.X%d-lock" % (n,n) )
 	return 0
 
     if os.access("/tmp/.X11-unix/X%d" % n, os.F_OK) :
-        print "Warning: display #%d is taken because of /tmp/.X11-unix/X%d" % (n,n)
+        print_err ( "Warning: display #%d is taken because of /tmp/.X11-unix/X%d" % (n,n) )
 	return 0
 
     return 1
@@ -46,7 +48,7 @@ def CheckDisplayNumber(n):
 def GetAvailableDisplay():
     for port in range(0,100):
         if (CheckDisplayNumber(port)):
-            print "Display #%d is available." % port
+            print_msg ( "Display #%d is available." % port )
 	    return port
 
 
@@ -60,13 +62,28 @@ def usage(code=0, msg=''):
         print >> fd, msg
     return code
 
+def print_err(msg):
+    global opt_verbose
+    if opt_verbose == 1:
+        print >> sys.stderr, msg
+
+def print_msg(msg):
+    global opt_verbose
+    if opt_verbose == 1:
+        print msg
+
+opt_verbose=1
+opt_check=0
+opt_available=0
+
 def main(argv=None):
+    global opt_verbose, opt_check, opt_available
     if argv is None:
         argv = sys.argv
     try:
         opts, args = getopt.getopt(
-            argv[1:], "hd:a",
-            ["help","port=","available"])
+            argv[1:], "hc:avq",
+            ["help","check=","available","verbose","quiet"])
     except getopt.error, msg:
         return usage(1, msg)
 
@@ -74,11 +91,21 @@ def main(argv=None):
         return usage(1)
 
     for opt, arg in opts:
+        if opt in ("-v", "--verbose"):
+	    opt_verbose = 1
+        elif opt in ("-q", "--quiet"):
+	    opt_verbose = 0
         if opt in ("-h", "--help"):
             return usage()
-        elif opt in ("-d", "--display"):
-	    return CheckDisplayNumber(arg)
+        elif opt in ("-c", "--check"):
+	    opt_check = 1
+	    var_check = arg
         elif opt in ("-a", "--available"):
+	    opt_available= 1
+
+    if opt_check == 1:
+	    return CheckDisplayNumber(var_check)
+    elif opt_available == 1:
 	    return GetAvailableDisplay()
 
 if __name__ == "__main__":
